f5782860f988ff0f913ddd90550e5ac85611e39e
Douglas <douglasonkeo3@gmail.com>
2022-06-23 19:54:09 +0300

    Sending Requests,  Responses by and Working with Postman

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,3 +1,18 @@
 exports.getPosts = (req, res, next) => {
-    
+    res.status(200).json({
+        "title": "Smokies", "content": "Here is the message"
+    })
 }
+
+// feed/post
+exports.createPost = (req, res, next) => {
+    const title = req.body.title;
+    const content = req.body.content;
+    // store db
+
+    res.status(201).json({
+        message: "Post created successfully",
+        post: {id: new Date().toISOString(), title2: title, content2: content}
+    })
+}
+
--- routes/feed.js
+++ routes/feed.js
@@ -5,6 +5,8 @@ const feedControllers = require('../controllers/feed')
 const router = express.Router();
 
 // GET /feed/posts
-router.get('/posts', feedControllers.getPosts)
+router.get('/posts', feedControllers.getPosts);
+
+router.post('/post', feedControllers.createPost);
 
 module.exports = router;
